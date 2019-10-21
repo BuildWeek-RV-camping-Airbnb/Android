5350ddf7765b19b82b5c83241e8d44a0babdfb39
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2019-10-21 14:57:40 -0600

    add an activity to reserve a place

---
+++ app/src/main/java/com/Lambda/rv_camping/ui/activities/ReservePlaceActivity.kt
@@ -0,0 +1,13 @@
+package com.Lambda.rv_camping.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.Lambda.rv_camping.R
+
+class ReservePlaceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reserve_place)
+    }
+}
