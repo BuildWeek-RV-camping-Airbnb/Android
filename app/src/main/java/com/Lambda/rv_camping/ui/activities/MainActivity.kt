d2e1de56d2e524d43972e60400abd226503510c0
RonnieDipple <Ronniedipple@gmail.com>
2019-10-22 17:52:49 -0500

    Merge branches 'UI' and 'development' of https://github.com/BuildWeek-RV-camping-Airbnb/Android into UI
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/Lambda/rv_camping/ui/activities/MainActivity.kt
+++ app/src/main/java/com/Lambda/rv_camping/ui/activities/MainActivity.kt
@@ -38,6 +38,7 @@ class MainActivity : AppCompatActivity() {
             adapter = RecyclerRVAdapter(campingList)
 
         }
+        
         // Could have also just use cl_activity_main_parent instead of container
             router = Conductor.attachRouter(this, container, savedInstanceState)
             if (!router.hasRootController()) {
