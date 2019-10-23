d2a2030ca7d02e643b2e80774e21ceeeff082954
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2019-10-23 13:18:23 -0600

    add toolbar
    toolbar to implement the menu later

--- app/src/main/java/com/Lambda/rv_camping/ui/controllers/MainController.kt
+++ app/src/main/java/com/Lambda/rv_camping/ui/controllers/MainController.kt
@@ -24,13 +24,22 @@ import com.bluelinelabs.conductor.ControllerChangeHandler
 import com.bluelinelabs.conductor.ControllerChangeType
 import com.bluelinelabs.conductor.RouterTransaction
 import com.bluelinelabs.conductor.changehandler.HorizontalChangeHandler
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.controller_login.view.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainController : Controller {
+class MainController : Controller, OnMapReadyCallback {
+
+    var mMap: GoogleMap? = null
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     companion object {
         val campingList = mutableListOf(
